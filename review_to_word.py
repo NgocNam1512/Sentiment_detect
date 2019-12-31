6a145c4841b008ba68dfe8f46e4ed2e2382251d9
NgocNam3518 <ngocnam3518@gmail.com>
2019-12-31 13:38:42 +0700

    try to read viet stopwords

---
+++ review_to_word.py
@@ -0,0 +1,30 @@
+import string
+
+def review_to_words(review):
+    """
+    Function to convert a raw review to a string of words
+    :param review
+    :return: meaningful_words
+    """
+    # 1. Convert to lower case, split into individual words
+    words = review.lower().split()
+    #
+    # 2. In Python, searching a set is much faster than searching
+    #   a list, so convert the stop words to a set
+    stops = set()
+    f = open("vie_stopwords.txt", "r", encoding="utf8")
+    for line in f:
+        word = line.strip()
+        stops.add(word)
+        print(stops)
+    #
+    # 3. Remove stop words
+    meaningful_words = [w for w in words if not w in stops]
+    #
+    # 4. Join the words back into one string separated by space,
+    # and return the result.
+    return " ".join(meaningful_words)
+
+
+if __name__ == "__main__":
+    review_to_words("Xin chào")
